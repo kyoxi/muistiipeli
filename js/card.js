c75b1c3d47c9d6009e0e1e2e0a85346ae2b22382
kyoxi <165962614+kyoxi@users.noreply.github.com>
2025-12-15 11:24:07 +0200

    Update card.js

--- js/card.js
+++ js/card.js
@@ -1,13 +1,12 @@
 export function createCardElement(card) {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
     cardElement.dataset.card = card;
     return cardElement;
 }
 
-export function flipCard(cardElement, callback) {
-    if (cardElement.classList.contains('flipped')) return;
-    cardElement.classList.add('flipped');
+export function flipCard(cardElement) {
+    if (cardElement.classList.contains("flipped")) return;
+    cardElement.classList.add("flipped");
     cardElement.textContent = cardElement.dataset.card;
-    callback(cardElement);
 }
