b9eeaa9d7939d509fe63de05e7b6a7980f6b642f
kyoxi <165962614+kyoxi@users.noreply.github.com>
2025-12-15 12:03:15 +0200

    Update card.js

--- js/card.js
+++ js/card.js
@@ -11,17 +11,6 @@ export function flipCard(cardElement) {
     cardElement.textContent = cardElement.dataset.card;
 }
 
-export function createCardElement(card) {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
 
-    const cardInner = document.createElement('span');
-    cardInner.classList.add('card-inner');
-    cardInner.textContent = card;
-
-    cardElement.appendChild(cardInner);
-    cardElement.dataset.card = card;
-    return cardElement;
-}
 
 
